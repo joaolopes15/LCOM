ec7ee17037024aec64fe40ef1c46547f6f1dd624
João Lopes <joaolopes15@users.noreply.github.com>
2025-04-02 11:12:17 +0100

    lab3 scan function done

--- lab3/lab3.c
+++ lab3/lab3.c
@@ -4,6 +4,15 @@
 
 #include <stdbool.h>
 #include <stdint.h>
+#include "keyboard.h" 
+
+#define MAKE_CODE 0x80
+#define BREAK_CODE 0x81
+#define TWO_BYTES 0xE0
+
+extern int counter_KBC;
+extern uint8_t scancode;
+extern bool expectingSecondByte;
 
 int main(int argc, char *argv[]) {
   // sets the language of LCF messages (can be either EN-US or PT-PT)
@@ -30,10 +39,48 @@ int main(int argc, char *argv[]) {
 }
 
 int(kbd_test_scan)() {
-  /* To be completed by the students */
-  printf("%s is not yet implemented!\n", __func__);
+  int ipc_status;
+  message msg;
+  uint8_t irq_set;
 
-  return 1;
+  if (keyboard_subscribe_int(&irq_set) != 0) return 1;
+
+  while (scancode != 0x81) {
+    
+    if ((driver_receive(ANY, &msg, &ipc_status)) != 0) {
+      printf("driver_receive failed\n");
+      continue;
+    }
+
+    if (is_ipc_notify(ipc_status)) {
+      switch (_ENDPOINT_P(msg.m_source)) {
+        case HARDWARE:
+          if (msg.m_notify.interrupts & irq_set) {
+            kbc_ih();
+            if (expectingSecondByte == false){
+              kbd_print_scancode(!(scancode & MAKE_CODE), scancode == TWO_BYTES ? 2 : 1, &scancode);
+            }
+            
+            //TODO: Exportar a variavel expectingSecondByte para o lab3.c
+            //Chamar kbc_print_scancode() é só chamar.
+            // MAS ATENCAO: Só chamas isto quando expectingSecondByte for false.
+            // POrque há casos que tens de esperar o segundo byte (se receber antes o 0xE0). E nesse caso não chamo logo o kbc_print_scancode()
+
+            // MAS: Para chamar kbd_print_scancode (bool make, uint8_t size, uint8_t *bytes)
+            // Tenho de configurar é make code. e Tenho de garantir que passo o size correto.
+          }
+          
+          break;
+        default:
+          break;
+      }
+    }
+  }
+
+  if (keyboard_unsubscribe_int() != 0) return 1;
+  if (kbd_print_no_sysinb(counter_KBC)) return 1;
+
+  return 0;
 }
 
 int(kbd_test_poll)() {
---
+++ lab3/utils.c
@@ -0,0 +1,37 @@
+#include <lcom/lcf.h>
+#include <stdint.h>
+
+extern int counter_KBC; 
+
+int(util_get_LSB)(uint16_t val, uint8_t *lsb) {
+  if (lsb == NULL)
+    return 1;
+  *lsb = 0xFF & val;
+  return 0;
+}
+
+int(util_get_MSB)(uint16_t val, uint8_t *msb) {
+  if (msb == NULL)
+    return 1;
+  *msb = 0xFF & (val >> 8);
+  return 0;
+}
+
+int(util_sys_inb)(int port, uint8_t *value) {
+  /* To be implemented by the students */
+  if (value == NULL)
+    return 1;   
+
+  uint32_t val;
+
+  #ifdef LAB3
+  counter_KBC++;
+  #endif 
+
+  int ret = sys_inb(port, &val); 
+  *value = 0xFF & val; 
+
+  return ret;
+
+  return 1;
+}
