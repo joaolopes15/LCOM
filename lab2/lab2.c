a8fffd791f397a34adc91a124f14630a65a5786f
Pedro Monteiro <218-up202307242@users.noreply.gitlab.up.pt>
2025-05-14 10:02:45 +0100

    Fixed timer 0 files

--- lab2/lab2.c
+++ lab2/lab2.c
@@ -32,31 +32,39 @@ int main(int argc, char *argv[]) {
 
 int(timer_test_read_config)(uint8_t timer, enum timer_status_field field) {
   uint8_t configuration;
-  if (timer_get_conf(timer, &configuration) != 0)
+
+  if (timer_get_conf(timer, &configuration) != 0) {
     return 1;
-  if (timer_display_conf(timer, configuration, field) != 0)
+  }
+
+  if (timer_display_conf(timer, configuration, field) != 0) {
     return 1;
+  }
+
   return 0;
 }
 
 int(timer_test_time_base)(uint8_t timer, uint32_t freq) {
-  if (freq < 19 || timer > 2)
-    return 1; 
-  if (timer_set_frequency(timer, freq) != 0)
+  if (freq < 19 || timer > 2) {
     return 1;
+  }
+
+  if (timer_set_frequency(timer, freq) != 0) {
+    return 1;
+  }
+
   return 0;
-  return 1;
 }
 
 int(timer_test_int)(uint8_t time) {
-  int ipc_status;
+  int ipc_status, r;
   message msg;
-  int r;
   uint8_t irq_set;
 
-  if (timer_subscribe_int(&irq_set) != 0)
-    return 1;
   // TODO: Subscribe timer 0 interrupts
+  if (timer_subscribe_int(&irq_set) != 0) {
+    return 1;
+  }
 
   while (time > 0) { /* You may want to use a different condition */
     /* Get a request message. */
@@ -71,7 +79,8 @@ int(timer_test_int)(uint8_t time) {
     if (is_ipc_notify(ipc_status)) { /* received notification */
       switch (_ENDPOINT_P(msg.m_source)) {
         case HARDWARE:
-          // TODO: Adaptar of IF aos interrupts do Timer                             /* hardware interrupt notification */
+          // TODO: Adaptar o IF aos interrupts do Timer
+          /* hardware interrupt notification */
           if (msg.m_notify.interrupts & irq_set) {
             timer_int_handler();
             if (contador % 60 == 0) {
@@ -94,7 +103,9 @@ int(timer_test_int)(uint8_t time) {
            /* no standard messages expected: do nothing */
     }
   }
-  if (timer_unsubscribe_int() != 0)
+  if (timer_unsubscribe_int() != 0) {
     return 1;
+  }
+  
   return 0;
 }
