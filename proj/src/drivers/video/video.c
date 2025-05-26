58375dc3f21d633f40c7fcc9139c9c3ded1475aa
Pedro Monteiro <218-up202307242@users.noreply.gitlab.up.pt>
2025-05-26 22:19:29 +0100

    changed from double buffer to triple buffer for performance increase and less input lag

--- proj/src/drivers/video/video.c
+++ proj/src/drivers/video/video.c
@@ -1,8 +1,10 @@
 #include "video.h"
 #include <lcom/lcf.h>
 
-static void *video_mem[2];
-static int videoBufferIndex = 0;
+static void *video_mem[3];
+static int currentDrawBuffer = 0;
+static int currentDisplayBuffer = 1;
+static int readyBuffer = 2;
 
 int(set_mode)(uint16_t mode) {
   reg86_t reg86;
@@ -30,13 +32,17 @@ int(flip)() {
   reg86.ah = 0x4F;
   reg86.al = 0x07;
   reg86.cx = 0;
-  reg86.dx = videoBufferIndex * vmi_p.YResolution;
+  reg86.dx = readyBuffer * vmi_p.YResolution;
 
   if (sys_int86(&reg86) != 0) {
     return 1;
   }
 
-  videoBufferIndex = (videoBufferIndex + 1) % 2;
+  int temp = currentDisplayBuffer;
+  currentDisplayBuffer = readyBuffer;
+  readyBuffer = currentDrawBuffer;
+  currentDrawBuffer = temp;
+  
   return 0;
 }
 
@@ -54,7 +60,7 @@ void *(vg_init) (uint16_t mode) {
   vram_size = vmi_p.XResolution * vmi_p.YResolution * ((vmi_p.BitsPerPixel + 7) / 8);
 
   mr.mr_base = (phys_bytes) vram_base;
-  mr.mr_limit = mr.mr_base + 2 * vram_size;
+  mr.mr_limit = mr.mr_base + 3 * vram_size;
 
   if (OK != (r = sys_privctl(SELF, SYS_PRIV_ADD_MEM, &mr))) {
     return NULL;
@@ -62,8 +68,9 @@ void *(vg_init) (uint16_t mode) {
 
   video_mem[0] = vm_map_phys(SELF, (void *) mr.mr_base, vram_size);
   video_mem[1] = vm_map_phys(SELF, (void *) (mr.mr_base + vram_size), vram_size);
+  video_mem[2] = vm_map_phys(SELF, (void *) (mr.mr_base + 2 * vram_size), vram_size);
 
-  if (video_mem[0] == MAP_FAILED || video_mem[1] == MAP_FAILED) {
+  if (video_mem[0] == MAP_FAILED || video_mem[1] == MAP_FAILED || video_mem[2] == MAP_FAILED) {
     return NULL;
   }
 
@@ -74,8 +81,9 @@ void *(vg_init) (uint16_t mode) {
   size_t buffer_size = vram_size;
   memset(video_mem[0], 0, buffer_size);
   memset(video_mem[1], 0, buffer_size);
+  memset(video_mem[2], 0, buffer_size);
 
-  return video_mem[videoBufferIndex];
+  return video_mem[currentDrawBuffer];
 }
 
 int(vg_draw_pixel)(uint16_t x, uint16_t y, uint32_t color) {
@@ -86,22 +94,22 @@ int(vg_draw_pixel)(uint16_t x, uint16_t y, uint32_t color) {
   unsigned int index = (y * vmi_p.XResolution + x);
 
   if (vmi_p.BitsPerPixel == 8) {
-    uint8_t *video_ptr = (uint8_t *) video_mem[videoBufferIndex];
+    uint8_t *video_ptr = (uint8_t *) video_mem[currentDrawBuffer];
     video_ptr[index] = (uint8_t) (color & 0xFF);
   }
   else if (vmi_p.BitsPerPixel == 15 || vmi_p.BitsPerPixel == 16) {
-    uint16_t *video_ptr = (uint16_t *) video_mem[videoBufferIndex];
+    uint16_t *video_ptr = (uint16_t *) video_mem[currentDrawBuffer];
     video_ptr[index] = (uint16_t) (color & 0xFFFF);
   }
   else if (vmi_p.BitsPerPixel == 24) {
-    uint8_t *video_ptr = (uint8_t *) video_mem[videoBufferIndex];
+    uint8_t *video_ptr = (uint8_t *) video_mem[currentDrawBuffer];
     index *= 3;
     video_ptr[index] = color & 0xFF;
     video_ptr[index + 1] = (color >> 8) & 0xFF;
     video_ptr[index + 2] = (color >> 16) & 0xFF;
   }
   else if (vmi_p.BitsPerPixel == 32) {
-    uint32_t *video_ptr = (uint32_t *) video_mem[videoBufferIndex];
+    uint32_t *video_ptr = (uint32_t *) video_mem[currentDrawBuffer];
     video_ptr[index] = color;
   }
 
@@ -163,7 +171,7 @@ int(draw_xpm)(xpm_map_t xpm, uint16_t x, uint16_t y) {
 }
 
 int(clear_screen)() {
-  if (memset(video_mem[videoBufferIndex], 0, vmi_p.XResolution * vmi_p.YResolution * ((vmi_p.BitsPerPixel + 7) / 8)) == NULL) {
+  if (memset(video_mem[currentDrawBuffer], 0, vmi_p.XResolution * vmi_p.YResolution * ((vmi_p.BitsPerPixel + 7) / 8)) == NULL) {
     return 1;
   }
 
