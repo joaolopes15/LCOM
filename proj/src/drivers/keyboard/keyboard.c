225913d5cf9ecb328d8f9c6c458a40510bdc5b28
Pedro Monteiro <218-up202307242@users.noreply.gitlab.up.pt>
2025-05-30 23:40:28 +0100

    keyboard comments done

--- proj/src/drivers/keyboard/keyboard.c
+++ proj/src/drivers/keyboard/keyboard.c
@@ -4,16 +4,20 @@ uint8_t scancode = 0;
 int hook = 1;
 bool has_e0 = false;
 
+// subscribes keyboard interrupts using the hook_id
 int(keyboard_subscribe_int)(uint8_t *bit_no) {
     if(bit_no == NULL) return 1;
     *bit_no = BIT(hook);
     return sys_irqsetpolicy(1, IRQ_REENABLE | IRQ_EXCLUSIVE, &hook);
 }
 
+// unsubscribes from keyboard interrupts
 int(keyboard_unsubscribe_int)() {
     return sys_irqrmpolicy(&hook);
 }
 
+
+// keyboard interrupt handler. reads the scancode from the KBC output buffer. if the read operation fails, the scancode is set to 0.
 void(kbc_ih)() {
     if(read_KBC_output(0x60, &scancode, 0) != 0) {
         scancode = 0;
@@ -21,6 +25,7 @@ void(kbc_ih)() {
     }
 }
 
+// function that restores the keyboard's configuration to enable interrupt generation
 int(keyboard_restore)() {
     uint8_t commandbyte;
 
